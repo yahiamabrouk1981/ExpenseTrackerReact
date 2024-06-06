57d57bd8608568f0ec29e731a2d27e87b57ba9b0
Yahia Mabrouk <yahiamabrouk1981@gmail.com>
2024-06-06 01:47:22 +0100

    first commit

---
+++ src/context/GlobalState.js
@@ -0,0 +1,35 @@
+import React, { createContext, useReducer } from "react";
+import App from "../App";
+import AppReducer from "./AppReducer";
+const intialState = {
+  transactions: [],
+};
+export const GlobalContext = createContext(intialState);
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, intialState);
+
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
